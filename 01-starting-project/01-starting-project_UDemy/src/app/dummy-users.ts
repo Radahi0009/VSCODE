6932fbb22062c4099d025bfd7916538de0396bb7
Radhakrishna Chikkala <chikkalaradhakrishna@gmail.com>
2024-11-25 14:22:55 -0600

    Updated dummy users ts and dummy images , so that we can use them in our project as dynamic data using string interpolation and random number

---
+++ 01-starting-project/01-starting-project_UDemy/src/app/dummy-users.ts
@@ -0,0 +1,32 @@
+export const DUMMY_USERS = [
+    {
+      id: 'u1',
+      name: 'Arun Manemalle',
+      avatar: 'user-1.jpg',
+    },
+    {
+      id: 'u2',
+      name: 'chaitanya Kotapalli',
+      avatar: 'user-2.jpg',
+    },
+    {
+      id: 'u3',
+      name: 'Khoa Nguyen',
+      avatar: 'user-3.jpg',
+    },
+    {
+      id: 'u4',
+      name: 'Kathik Gothrala',
+      avatar: 'user-4.jpg',
+    },
+    {
+      id: 'u5',
+      name: 'Simba Lion',
+      avatar: 'user-5.jpg',
+    },
+    {
+      id: 'u6',
+      name: 'Shubham Gupta',
+      avatar: 'user-6.jpg',
+    },
+  ];
