57b200dfa2d5af1a3df39386be6332161f4396d0
Radhakrishna Chikkala <chikkalaradhakrishna@gmail.com>
2024-11-25 13:48:33 -0600

    uPDATED THE COMPONENTS.TS FILE WITH imports of user file, user component and user service, updated the app component css file

--- 01-starting-project/01-starting-project_UDemy/src/app/app.component.ts
+++ 01-starting-project/01-starting-project_UDemy/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { UserComponent } from "./user/user.component";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  imports: [HeaderComponent],
+  
    standalone: true, 
- 
-  styleUrl: './app.component.css',
+   templateUrl: './app.component.html', // Path to the HTML template file
+   styleUrl: './app.component.css', // Path to the CSS file
+  imports: [HeaderComponent, UserComponent]
+  
 })
 export class AppComponent {}
